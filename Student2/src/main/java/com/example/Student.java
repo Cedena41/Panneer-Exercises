a2d226039703f5f1873018f0be3b46f37184acc7
Cedena41 <carlosromhdez@hotmail.com>
2017-01-26 15:54:47 +0100

    Add files via upload

---
+++ Student2/src/main/java/com/example/Student.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Student {
+	String name;
+	String city;
+	
+	public String getName() {
+		return name;
+		
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Student2/src/main/java/com/example/Student2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Student2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Student2Application.class, args);
+	}
+}
